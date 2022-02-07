22987ac36dc3d55dad9dd22e0cdb8fb56ced26f1
giovanniammirati <93709144+giovanniammirati@users.noreply.github.com>
2022-02-07 20:29:22 +0100

    Add files via upload

---
+++ src/me/mrcoder/commands/PUGUICmd.java
@@ -0,0 +1,18 @@
+package me.mrcoder.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+
+public class PUGUICmd implements Listener,CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender s, Command cmd, String alias, String[] args) {
+		s.sendMessage("§r");
+		s.sendMessage("§3PickaxeUpgradesGUI §bby MrCoder_ on Fiverr.");
+		s.sendMessage("§7§o(( §f§oRight click with a pickaxe to open. §7§o))§r");
+		s.sendMessage("§r");
+		return true;
+	}
+}
